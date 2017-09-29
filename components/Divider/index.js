1d3a857b3529f64d592d2e051b772521d3628a62
Jannis Pohlmann <contact@jannispohlmann.de>
2017-09-29 11:36:39 +0200

    utils/styles: Improve spacing around dividers

--- components/Divider/index.js
+++ components/Divider/index.js
@@ -3,6 +3,6 @@ import styled from 'styled-components'
 import { Colors } from '../../utils/styles'
 
 export default styled.hr`
-  margin: 2em 0em;
+  margin: 3em 0em;
   background-color: ${Colors.fifth};
 `
